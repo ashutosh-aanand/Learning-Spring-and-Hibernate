e7f73c4ec83a0c0a1f520090286703aeaf126432
Ashutosh Anand <ashutosh.aanand@gmail.com>
2022-03-06 17:39:58 +0530

    created CreateCourseAndReviews.java file

---
+++ 9.hibernate-one-to-many-uni/src/com/learn/onetomany/demo/CreateCourseAndReviews.java
@@ -0,0 +1,45 @@
+package com.learn.onetomany.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.learn.onetomany.entity.Course;
+import com.learn.onetomany.entity.Instructor;
+import com.learn.onetomany.entity.InstructorDetail;
+
+public class CreateCourseAndReviews {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+									.configure()
+									.addAnnotatedClass(Course.class)
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.buildSessionFactory();
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			// start a transaction ------------------------------------------------------
+			session.beginTransaction();
+			
+			
+
+			// commit transaction --------------------------------------------------------
+			session.getTransaction().commit();
+			
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			// cleanup code
+			session.close();
+			factory.close();
+		}
+		
+	}
+
+}
